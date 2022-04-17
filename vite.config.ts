8ddf6dafefc19035d7550e0a688bdf7d2a5f0366
Luca Pagliaro <pagliaroluca@gmail.com>
2022-04-17 23:45:20 +0200

    feat: added webp

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import ViteImages from 'vite-plugin-vue-images'
+
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), vue()],
+  plugins: [
+    tsconfigPaths(),
+    vue(),
+    ViteImages({
+      dirs: ['src/assets'],
+    })
+  ],
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`
