64cdc75138de8e4605329912713ea7ad163b736d
Luca Pagliaro <pagliaroluca@gmail.com>
2022-04-19 09:29:03 +0200

    feat: added GTAG

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import App from './App.vue'
 
 export const createApp = ViteSSG(App, (ctx) => {
   ctx.app.use(MotionPlugin);
-  ctx.app.use(VueGtag, {
-    config: {id: "UA-28358707-9"}
-  })
+
+  if (import.meta.env.GTAG_ID) {
+    ctx.app.use(VueGtag, {
+      config: {id: import.meta.env.GTAG_ID}
+    })
+  }
 })
