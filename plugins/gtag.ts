842a8b6a4e0d7118c140a76f25c033b6801d22f1
Luca Pagliaro <pagliaroluca@gmail.com>
2022-05-03 21:06:35 +0200

    fix: gtm id

--- plugins/gtag.ts
+++ plugins/gtag.ts
@@ -4,7 +4,7 @@ import {defineNuxtPlugin} from "#app";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'GTM-WQXMBWR'
+      id: 'G-PXR3HDSPNY'
     }
   })
 })
