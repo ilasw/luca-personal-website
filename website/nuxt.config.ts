516a9f05b36e4c768f7643e522db0361c23a07c7
Luca Pagliaro <luca@sentiero.digital>
2024-04-30 16:01:58 +0200

    feat: added new favicon

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {enabled: process.env.NODE_ENV === 'development'},
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/motion/nuxt', ['@nuxtjs/google-fonts', {
-    families: {
-      'Work+Sans': [400, 500, 700],
-    },
-    display: 'swap'
-  }], "@nuxt/image"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/motion/nuxt',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Work+Sans': [400, 500, 700],
+      },
+      display: 'swap'
+    }],
+    "@nuxt/image"
+  ],
   routeRules: {
     '/en': {redirect: '/'},
-  }
+  },
+  ssr: true,
 })
