e45cbccdfba91a7ccc694398da4a239a3b11f6af
Luca Pagliaro <luca@sentiero.digital>
2024-04-30 16:28:41 +0200

    chore: updated node version

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,6 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: false,
+  devtools: {enabled: false},
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/motion/nuxt',
