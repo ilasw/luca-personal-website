8730fdd086d8df79308f051c97cdcb4ce183f286
Luca Pagliaro <luca@sentiero.digital>
2024-05-09 16:46:56 +0200

    feat: added analytics

--- website/plugins/gtag.ts
+++ website/plugins/gtag.ts
@@ -3,11 +3,9 @@ import VueGtag from 'vue-gtag'
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
-  if (config.public.gtagId) {
-    nuxtApp.vueApp.use(VueGtag, {
-      config: {
-        id: `GTM-WQXMBWR`
-      }
-    })
-  }
+  nuxtApp.vueApp.use(VueGtag, {
+    config: {
+      id: `GTM-WQXMBWR`
+    }
+  })
 })
