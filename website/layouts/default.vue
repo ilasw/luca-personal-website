1887af9337dd168d215fcac1a37684cdb4f4663f
Luca Pagliaro <luca@sentiero.digital>
2024-06-11 11:09:41 +0200

    fix: updated metas

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -20,12 +20,17 @@ useHead({
   htmlAttrs() {
     return {
       lang: 'en',
-      description: t('seo.description'),
-      ogDescription: t('seo.description'),
-      ogImage: ogImage,
-      twitterCard: 'summary_large_image',
-      keywords: t('seo.keywords'),
     }
+  },
+  meta() {
+      return [
+        {charset: 'utf-8'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {hid: 'og:title', property: 'og:title', content: t('seo.title')},
+        {hid: 'description', name: 'description', content: t('seo.description')},
+        {hid: 'og:description', property: 'og:description', content: t('seo.description')},
+        {hid: 'og:image', property: 'og:image', content: ogImage},
+      ]
   },
 })
 </script>
