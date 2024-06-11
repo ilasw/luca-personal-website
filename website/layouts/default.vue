31e1ece4afad084d67947cfb4fc8107f24d33784
Luca Pagliaro <luca@sentiero.digital>
2024-06-11 11:04:27 +0200

    fix: updated metas

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -12,13 +12,19 @@
 import Header from "~/components/layout/Header.vue";
 import Footer from "~/components/layout/Footer.vue";
 import ogImage from "~/public/images/me/luca-working-bormio.png";
+import {useLocale} from "~/hooks/useLocale";
+
+const {t} = useLocale();
 
 useHead({
   htmlAttrs() {
     return {
       lang: 'en',
+      description: t('seo.description'),
+      ogDescription: t('seo.description'),
       ogImage: ogImage,
       twitterCard: 'summary_large_image',
+      keywords: t('seo.keywords'),
     }
   },
 })
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import {useLocale} from "~/hooks/useLocale";
 import Hero from "~/components/home/Hero.vue";
 import Clients from "~/components/home/ClientLogos.vue";
 import ServiceGrid from "~/components/home/ServiceGrid.vue";
 import AboutMe from "~/components/home/AboutMe.vue";
-
-const {t} = useLocale()
 </script>
 
 <template>
-  <Head>
-    <Title>{{ t('seo.title') }}</Title>
-    <Meta name="description" content="{{ t('seo.description') }}"/>
-    <Meta property="og:description" content="{{ t('seo.description') }}"/>
-    <Meta name="keywords" content="{{ t('seo.keywords') }}"/>
-  </Head>
-
   <Hero/>
   <Clients/>
   <ServiceGrid/>
